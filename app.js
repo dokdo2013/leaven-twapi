6a98d26e772dcf865d7ccbb56f809b673a43c0de
hyeonwoojo <h_jo@ejn.gg>
2022-09-10 18:58:54 +0900

    feat: add code from best practice

--- app.js
+++ app.js
@@ -10,6 +10,29 @@ const { get_app_access_token } = require("./twitch");
 
 const app = express();
 
+// Notification request headers
+const TWITCH_MESSAGE_ID = "Twitch-Eventsub-Message-Id".toLowerCase();
+const TWITCH_MESSAGE_TIMESTAMP =
+  "Twitch-Eventsub-Message-Timestamp".toLowerCase();
+const TWITCH_MESSAGE_SIGNATURE =
+  "Twitch-Eventsub-Message-Signature".toLowerCase();
+const MESSAGE_TYPE = "Twitch-Eventsub-Message-Type".toLowerCase();
+
+// Notification message types
+const MESSAGE_TYPE_VERIFICATION = "webhook_callback_verification";
+const MESSAGE_TYPE_NOTIFICATION = "notification";
+const MESSAGE_TYPE_REVOCATION = "revocation";
+
+// Prepend this string to the HMAC that's created from the message
+const HMAC_PREFIX = "sha256=";
+
+app.use(
+  express.raw({
+    // Need raw message body for signature verification
+    type: "application/json",
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -31,28 +54,45 @@ app.delete("/eventsub/:id", async (req, res) => {
   res.send(await delete_eventsub(req.params.id));
 });
 
-app.get("/eventsub/callback", async (req, res) => {
-  const data = req.body || {};
-  console.log(data);
-
-  // if header have 'Twitch-Eventsub-Message-Type', then return data.challenge
-  if (req.headers["twitch-eventsub-message-type"]) {
-    res.send(data.challenge);
-  } else {
-    res.send(data);
-  }
-});
-
 app.post("/eventsub/callback", async (req, res) => {
-  console.log("is post");
-  const data = req.body || {};
-  console.log(data);
+  let secret = process.env.TWITCH_WEBHOOK_SECRET;
+  let message = getHmacMessage(req);
+  let hmac = HMAC_PREFIX + getHmac(secret, message); // Signature to compare
 
-  // if header have 'Twitch-Eventsub-Message-Type', then return data.challenge
-  if (req.headers["Twitch-Eventsub-Message-Type"] || req.body?.challenge) {
-    res.send(data.challenge);
+  if (true === verifyMessage(hmac, req.headers[TWITCH_MESSAGE_SIGNATURE])) {
+    console.log("signatures match");
+
+    // Get JSON object from body, so you can process the message.
+    let notification = JSON.parse(req.body);
+
+    if (MESSAGE_TYPE_NOTIFICATION === req.headers[MESSAGE_TYPE]) {
+      // TODO: Do something with the event's data.
+
+      console.log(`Event type: ${notification.subscription.type}`);
+      console.log(JSON.stringify(notification.event, null, 4));
+
+      res.sendStatus(204);
+    } else if (MESSAGE_TYPE_VERIFICATION === req.headers[MESSAGE_TYPE]) {
+      res.status(200).send(notification.challenge);
+    } else if (MESSAGE_TYPE_REVOCATION === req.headers[MESSAGE_TYPE]) {
+      res.sendStatus(204);
+
+      console.log(`${notification.subscription.type} notifications revoked!`);
+      console.log(`reason: ${notification.subscription.status}`);
+      console.log(
+        `condition: ${JSON.stringify(
+          notification.subscription.condition,
+          null,
+          4
+        )}`
+      );
+    } else {
+      res.sendStatus(204);
+      console.log(`Unknown message type: ${req.headers[MESSAGE_TYPE]}`);
+    }
   } else {
-    res.send(data);
+    console.log("403"); // Signatures didn't match.
+    res.sendStatus(403);
   }
 });
 
@@ -64,3 +104,25 @@ app.get("/token", async (req, res) => {
 app.listen(4200, () => {
   console.log("Example app listening on port 4200!");
 });
+
+// Build the message used to get the HMAC.
+function getHmacMessage(request) {
+  return (
+    request.headers[TWITCH_MESSAGE_ID] +
+    request.headers[TWITCH_MESSAGE_TIMESTAMP] +
+    request.body
+  );
+}
+
+// Get the HMAC.
+function getHmac(secret, message) {
+  return crypto.createHmac("sha256", secret).update(message).digest("hex");
+}
+
+// Verify whether our hash matches the hash that Twitch passed in the header.
+function verifyMessage(hmac, verifySignature) {
+  return crypto.timingSafeEqual(
+    Buffer.from(hmac),
+    Buffer.from(verifySignature)
+  );
+}

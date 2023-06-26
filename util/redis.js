16a18428f22e689df72d949a4f5f73fe2d28af3b
hyeonwoojo <h_jo@ejn.gg>
2023-06-26 18:28:08 +0900

    feat: add password

--- util/redis.js
+++ util/redis.js
@@ -6,6 +6,7 @@ const redisClient = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
   },
+  password: process.env.REDIS_PASSWORD,
 });
 
 const connect = async () => {
